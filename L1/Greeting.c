206e13d8e7deb1a30eac81440ba648f4a1d288d2
Abdul Sami <physicskabaap2025@gmail.com>
2026-03-03 22:52:07 +0530

    Delete L1 directory

--- L1/Greeting.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Welcome To Programming !\n");
-    printf("We are Programming in C !\n");
-    return 0;
-}
--- L1/Hello.c
+++
@@ -1,9 +0,0 @@
-// Including Standard Input & Output Header
-#include <stdio.h>
-
-// Program Entry Point 
-int main(void)
-{
-    printf("C --> Hello World !\n");
-    return 0;
-}
